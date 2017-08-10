0785014da1c00352237abe8bcae833305b6ab02b
Paul Gross <pgross@maryville.edu>
2017-08-09 21:14:20 -0500

    Init

--- Light Color Controller/ViewController.swift
+++ Light Color Controller/ViewController.swift
@@ -20,6 +20,57 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBOutlet weak var hueValueText: UITextField!
+    @IBOutlet weak var colorNameLabel: UILabel!
+    @IBOutlet weak var switchButton: UIButton!
 
+    var isLightOn = false
+    
+    @IBAction func onSwitchLight(_ sender: Any) {
+        isLightOn = !isLightOn
+        
+        if isLightOn {
+            if let hueValueString = hueValueText.text {
+                if let hueValue = Float( hueValueString ) {
+                    let huePercent = hueValue / 360.0
+                    let lightColor = UIColor( hue: CGFloat(huePercent), saturation: 1.0, brightness: 1.0, alpha: 1.0)
+                    
+                    view.backgroundColor = lightColor
+                    
+                    
+                    switchButton.setTitle("OFF", for: UIControlState.normal)
+                    
+                    let nameLabelColor = UIColor( hue: CGFloat(huePercent), saturation: 0.5, brightness: 0.5, alpha: 1.0)
+                    colorNameLabel.backgroundColor = nameLabelColor
+                    
+                    let colorName:String
+                    
+                    switch hueValue {
+                        case 30...89: colorName = "YELLOW"
+                        case 90...149: colorName = "GREEN"
+                    case 150...209: colorName = "CYAN"
+                    case 210...269: colorName = "BLUE"
+                    case 270...329: colorName = "MAGENTA"
+                    case 0...29, 330...360: colorName = "RED"
+                    default:
+                        colorName = "INVALID COLOR"
+                    }
+                    
+                    
+                    colorNameLabel.text = colorName
+                    
+                    
+                    
+                }
+            }
+        } else {
+            view.backgroundColor = .white
+            switchButton.setTitle("ON", for: UIControlState.normal)
+            
+            colorNameLabel.text = ""
+            colorNameLabel.backgroundColor = .white
+        }
+    }
 }
 
